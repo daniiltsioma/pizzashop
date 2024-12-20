555d282a9d12f2b1e68a9d9f4d4e59d012b015fa
dtsiomapdx <dev.dtsioma@gmail.com>
2024-12-20 14:06:06 -0800

    remote unnecessary orders

--- pizzashop/settings.py
+++ pizzashop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'orders',
     'store',
     'rest_framework'
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'pizzashop.urls'
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
--- pizzashop/urls.py
+++ pizzashop/urls.py
@@ -13,9 +13,6 @@ router.register('stores', StoreViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/orders/add/', add_order, name='add_order'),
-    path('api/orders/delete/<int:order_id>', delete_order, name='delete_order'),
     path('api/orders/edit-status/<int:order_id>', edit_order_status, name='edit-status'),
-    path('api/pizzas/delete/<int:pizza_id>', delete_pizza, name='delete_pizza'),
     path('api/stores/<int:store_id>/orders', StoreOrdersListView.as_view(), name='store-orders')
 ]
