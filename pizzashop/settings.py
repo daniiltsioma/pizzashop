25408acce9c5984e7cc2ae96088b478456ed4f6d
dtsiomapdx <dev.dtsioma@gmail.com>
2024-12-23 22:15:57 -0800

    simplification

--- pizzashop/settings.py
+++ pizzashop/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'orders',
-    'store',
+    'pizzas',
     'rest_framework'
 ]
 
--- pizzashop/urls.py
+++ pizzashop/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from orders.views import PizzaViewSet, OrderViewSet, ToppingViewSet, edit_order_status
-from store.views import EmployeeViewSet, StoreOrdersListView, StoreViewSet
+
+from pizzas.views import PizzaViewSet
 
 router = routers.DefaultRouter()
 router.register('pizzas', PizzaViewSet)
-router.register('orders', OrderViewSet)
-router.register('toppings', ToppingViewSet)
-router.register('employees', EmployeeViewSet)
-router.register('stores', StoreViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/orders/edit-status/<int:order_id>', edit_order_status, name='edit-status'),
-    path('api/stores/<int:store_id>/orders', StoreOrdersListView.as_view(), name='store-orders')
 ]
